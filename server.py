7a3ae8186f1d6b6a5b3d2b97fba924b18cfb1d4a
Linda Li <zl3143@columbia.edu>
2022-04-16 15:45:48 -0400

    read question data from backend

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ user_answers = {
     "7" "8" "9"
     part 2 will be a dictionary with cell number as key, candidate array as value
         eg. "1": [2, 3, 4]'''
-solution = {
+solutions = {
     "1":{
         "part1":{
             "pattern": "",
@@ -108,6 +108,44 @@ solution = {
     }
 }
 
+'''
+    store questions
+    arrangement is an array representing filled cells. For unfilled cells, use 0 instead
+    candidates is an array or array representing candidates of untill cells
+    format is tuple representing the number of rows and columns
+'''
+questions = {
+    "1":{
+        "arrangement": [0,0,7,0,0,0,0,0,0],
+        "candidates": [[5,8,9],[8,9],[],[1,2,4,8,9],[1,2,4,8,9],[8,9],[4,5,6,8,9],[3,4,6,8,9],[3,5,8,9]],
+        "format": (3,3),
+    },
+    "2":{
+        "arrangement": [],
+        "candidates": [],
+        "format": (),
+    },
+    "3":{
+        "arrangement": [],
+        "candidates": [],
+        "format": (),
+    },
+    "4":{
+        "arrangement": [],
+        "candidates": [],
+        "format": (),
+    },
+    "5":{
+        "arrangement": [],
+        "candidates": [],
+        "format": (),
+    },
+    "6":{
+        "arrangement": [],
+        "candidates": [],
+        "format": (),
+    },
+}
 
 @app.route('/')
 def homepage():
@@ -126,22 +164,34 @@ def quiz():
 
 @app.route('/quiz/<id>/part1', methods=['GET', 'POST'])
 def quiz_part1(id):
-    return render_template('quiz_part1.html', id=id)
+    for key, value in questions.items():
+        if key == str(id):
+            question = value
+    return render_template('quiz_part1.html', id=id, question=question)
 
 
 @app.route('/answer/<id>/part1', methods=['GET', 'POST'])
 def answer_part1(id):
-    return render_template('quiz_part1_answer.html')
+    for key, value in solutions.items():
+        if key == str(id):
+            solution = value
+    return render_template('quiz_part1_answer.html', id=id, solution=solution)
 
 
 @app.route('/quiz/<id>/part2', methods=['GET', 'POST'])
 def quiz_part2(id):
-    return render_template('quiz_part2.html')
+    for key, value in questions.items():
+        if key == str(id):
+            question = value
+    return render_template('quiz_part2.html', id=id, question=question)
 
 
 @app.route('/answer/<id>/part1', methods=['GET', 'POST'])
 def answer_part2(id):
-    return render_template('quiz_part2_answer.html')
+    for key, value in solutions.items():
+        if key == str(id):
+            solution=value
+    return render_template('quiz_part2_answer.html', id=id, solution=solution)
 
 
 if __name__ == '__main__':
